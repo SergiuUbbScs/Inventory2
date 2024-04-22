d2be15f116db067acd72e17437f4f4d83013bfc0
beatrice2802 <126410861+beatrice2802@users.noreply.github.com>
2024-04-22 22:49:06 +0300

    lab4

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module inventory {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.logging;
+
+    opens inventory.model to javafx.base;
+    exports inventory.model;
+    opens inventory to javafx.fxml;
+    exports inventory;
+    opens inventory.controller to javafx.fxml;
+    exports inventory.controller;
+}
